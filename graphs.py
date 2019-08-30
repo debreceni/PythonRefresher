36c9d6e48b53432a55719050c6fd781844d33aad
francescoafabozzi <francescoafabozzi@gmail.com>
2019-08-30 13:44:22 -0400

    Adding Labels

--- graphs.py
+++ graphs.py
@@ -1,13 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-x = np.arange(0, 2*np.pi, .25*np.pi)
+x = np.arange(0, 2*np.pi, .1)
 y = np.sin(x)
 z = np.cos(x)
 zero = [0 for i in range(len(x))]
 
 plt.title('One Period Trig Functions Plot')
-plt.plot(x,y)
-plt.plot(x,z)
+plt.plot(x,y, label='Sin(x)')
+plt.plot(x,z, label='Cos(x)')
 plt.hlines(y = 0, xmin=0, xmax=2*np.pi, linestyles='--')
+plt.legend()
 plt.show()
